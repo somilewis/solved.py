175a39aad453237fed5ca8f3c4e91a8cfa596cab
somilewis <87447136+somilewis@users.noreply.github.com>
2023-06-02 07:50:09 -0700

    Add files via upload

---
+++ LAB 2.py
@@ -0,0 +1,34 @@
+# Lewis Muthomi
+# 1250 01
+# 08-31-2022
+# lab 2
+
+# This program gives solution to powers of two for eight different variables
+
+# User greeting
+print('welcome to powers of 2 program')
+print()
+
+ # add to 2
+firstAnswer = 2 ** 1
+secondAnswer = 2 ** 2
+thirdAnswer = 2 ** 3
+fourthAnswer = 2 ** 4
+fifthAnswer = 2 ** 5
+sixthAnswer = 2 ** 6
+seventhAnswer = 2 ** 7
+eighthAnswer = 2 ** 8
+
+# display results
+print('2 raised to the power of 1 equals:', firstAnswer)
+print('2 raised to the power of 2 equals:', secondAnswer)
+print('2 raised to the power of 3 equals:', thirdAnswer)
+print('2 raised to the power of 4 equals:', fourthAnswer)
+print('2 raised to the power of 5 equals:', fifthAnswer)
+print('2 raised to the power of 6 equals:', sixthAnswer)
+print('2 raised to the power of 7 equals:', seventhAnswer)
+print('2 raised to the power of 8 equals:', eighthAnswer)
+print()
+
+# exit display
+print('have a nice day!')
